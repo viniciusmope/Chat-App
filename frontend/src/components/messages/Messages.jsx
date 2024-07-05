177d590103509da45f52d40cada4e215ad8d4e73
Vinicius Moreira <viniciuspemo1@gmail.com>
2024-07-05 02:26:18 -0300

    build: frontend completo

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from "react";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
+import Message from "./Message";
+import useListenMessages from "../../hooks/useListenMessages";
+
+// O QUE PERMITE RECEBER MENSAGENS
+const Messages = () => {
+	const { messages, loading } = useGetMessages();
+	useListenMessages();
+	const lastMessageRef = useRef();
+
+	useEffect(() => {
+		setTimeout(() => {
+			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+		}, 100);
+	}, [messages]);
+
+	return (
+		<div className='px-4 flex-1 overflow-auto'>
+			{!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} ref={lastMessageRef}>
+						<Message message={message} />
+					</div>
+				))}
+
+			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+			{!loading && messages.length === 0 && (
+				<p className='text-center'>Envie uma mensagem para começar a conversa</p>
+			)}
+		</div>
+	);
+};
+export default Messages;
